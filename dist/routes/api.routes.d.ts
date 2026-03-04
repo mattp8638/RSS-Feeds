132534c4fa8a723e19047418a0ccd31e535dda87
mattp8638 <mattp8638@gmail.com>
2026-03-04 15:50:44 +0000

    Delete dist directory

--- dist/routes/api.routes.d.ts
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import { FeedService } from '../services/feed.service';
-import { DatabaseService } from '../services/database.service';
-export default function apiRoutes(feedService: FeedService, dbService: DatabaseService): Router;
-//# sourceMappingURL=api.routes.d.ts.map
--- dist/services/api.routes.d.ts
+++
@@ -1,4 +0,0 @@
-import { Router } from 'express';
-import { FeedService } from '../services/feed.service';
-export default function apiRoutes(feedService: FeedService): Router;
-//# sourceMappingURL=api.routes.d.ts.map
